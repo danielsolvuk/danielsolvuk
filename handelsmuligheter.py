ec5795564e527f6ff3a50b5ffc0203b05e528186
danielsolvuk <danielsolvik@yahoo.com>
2022-03-07 07:56:49 +0100

    Update handelsmuligheter.py

--- handelsmuligheter.py
+++ handelsmuligheter.py
@@ -134,19 +134,6 @@ def get_data_from_kortsikt(count, columns):
 
     print(datetime_obj, trendgulv, trendtak, rsi)
 
-    # return  {
-    #     'Date': datetime_obj,
-    #     'Name': columns[count]['name'],
-    #     'Ticker': columns[count]['ticker'], 
-    #     'Days_since': columns[count]['dager_siden'],
-    #     'Pivotpunkt': columns[count]['pivotpunkt'],
-    #     'Days_since_id': columns[count]['dager_siden_id'],
-    #     'Signal': columns[count]['signal'],
-    #     'Trendgulv': trendgulv, 
-    #     'Trendtak': trendtak, 
-    #     'Rsi': rsi,
-    # } 
-
     return {
             'Date': datetime_obj,
             'Name': columns[count]['name'],
@@ -159,56 +146,6 @@ def get_data_from_kortsikt(count, columns):
             'Potensial': columns[count]['potensial']
         } 
 
-# def get_data_from_kortsikt(count, columns):
-
-#     # Function to split a string
-#     splitString = lambda row, line, numb: row.text.splitlines()[line].split()[numb]
-
-#     try:
-#         # Find header
-#         header = driver.find_element_by_xpath('//*[@id="ca2017_HeadNameTicker"]').text 
-        
-#         # Find subheader
-#         subheader = driver.find_element_by_xpath('//*[@id="ca2017_HeadPriceAndDateInfo"]').text
-        
-#         # Date attributes
-#         year = subheader.split()[-1]
-#         month = subheader.split()[-2]
-#         day = subheader.split()[-3].replace('.', '')
-
-#         # Create date from the date attributes
-#         datetime_object = datetime.datetime.strptime(f'{year} {month} {day}', '%Y %b %d').date()
-
-#         # Find the first line for trendgulv and trendtak
-#         row_1 = driver.find_element_by_xpath('//*[@id="ca2017_QuantIndicatorsTable"]/tbody/tr[1]/td')
-#         # Find the third line for rsi
-#         row_2 = driver.find_element_by_xpath('//*[@id="ca2017_QuantIndicatorsTable"]/tbody/tr[3]/td')
-#         # Get only the ticker from header
-#         ticker = header.replace('(','').replace(')','').replace('.', ' ').split()[-2]
-#         # Get only the trendgulv value from first row
-#         trendgulv = splitString(row_1, 1, 2)
-#         # Get only the trendtak value from first row
-#         trendtak = splitString(row_1, 2, 2)
-#         # Get RSI
-#         rsi = splitString(row_2, 1, -1)
-
-#         # Put the data together 
-#         data = {
-#             'Date': datetime_object,
-#             'Name': columns[count]['name'],
-#             'Ticker': ticker, 
-#             'Trendgulv': trendgulv, 
-#             'Trendtak': trendtak, 
-#             'Rsi': rsi,
-#             'Handelsmulighetstype': columns[count]['handelstype'],
-#             'Riskreward': columns[count]['riskreward'],
-#             'Potensial': columns[count]['potensial']
-#         } 
-
-#         return data
-
-#     except Exception as e:
-#         print("error", e)
 
 
 def get_columns_from_each_entry_in_list(table, rows):
